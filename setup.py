eca5ec0df6944ce26067a0a69785c36d5b30624b
Rich Jones <rich@anomos.info>
2014-03-13 17:11:33 -0700

    pakajin

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-easy-split',
+    version='0.0.1',
+    packages=['easy_split'],
+    install_requires=[''],
+    include_package_data=True,
+    license='MIT License',
+    description='Super easy split testing for your Django projects.',
+    long_description=README,
+    url='https://github.com/Miserlou/django-easy-split',
+    author='Rich Jones',
+    author_email='rich@openwatch.net',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
